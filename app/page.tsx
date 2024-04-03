d8bde67dad030f7e5aa8b74464d56a9594efaea8
Chandan Das <chandan@Mac.local>
2024-04-03 23:47:13 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,83 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.base}>
+      <header className={styles.baseInner}>
+        <div className={styles.frameParent}>
+          <div className={styles.lineWrapper}>
+            <div className={styles.frameChild} />
+          </div>
+          <div className={styles.socialMediaIcons}>
+            <h1 className={styles.mayaverse}>MayaVerse</h1>
+            <nav className={styles.socialMediaIconsInner}>
+              <nav className={styles.aboutUsParent}>
+                <div className={styles.aboutUs}>About Us</div>
+                <div className={styles.guide}>Guide</div>
+                <Link href={"https://linktr.ee/mayaverse"} target="_blank">
+                  <div className={styles.rooms}>Socials</div>
+                </Link>
+                <div className={styles.blogs}>Blogs</div>
+              </nav>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <main className={styles.bgParent}>
+        <img
+          className={styles.bgIcon}
+          alt=""
+          src="https://gateway.ipfs.io/ipfs/QmZJ3qQ4J86vATRgyAsUbSqgpYtZV1Beza6dqcbZTeBDJM"
+        />
+        <img
+          className={styles.twitterIcon}
+          loading="lazy"
+          alt=""
+          src="https://gateway.ipfs.io/ipfs/QmQz38YnDbyGCdkCu8jYyytANyDVUtxbhksUDEF64guEjY"
+        />
+        <img
+          className={styles.instagramIcon}
+          loading="lazy"
+          alt=""
+          src="https://gateway.ipfs.io/ipfs/QmQz38YnDbyGCdkCu8jYyytANyDVUtxbhksUDEF64guEjY"
+        />
+        <img
+          className={styles.style3Copy4}
+          loading="lazy"
+          alt=""
+          src="https://gateway.ipfs.io/ipfs/QmaCpqZxd5SC7sJ2UvihBjoa3Q8LH7pMwL5xFqsVengYXC"
+        />
+      </main>
+      <div className={styles.sayMyNameWrapper}>
+        <h1 className={styles.sayMyNameContainer}>
+          <span className={styles.sayMyNameContainer1}>
+            <p className={styles.say}>Say</p>
+            <p className={styles.my}> My</p>
+            <p className={styles.name}> Name</p>
+          </span>
+        </h1>
+      </div>
+      <div className={styles.createAccountLoginWrapper}>
+        <div className={styles.createAccountLogin}>
+          <button className={styles.buttonRegister}>
+            <div className={styles.buttonRegisterChild} />
+            <div className={styles.createAccount}>Create Account</div>
+          </button>
+          <button className={styles.buttonLogin}>
+            <div className={styles.buttonLoginChild} />
+            <div className={styles.login}>Login</div>
+          </button>
+        </div>
+      </div>
+      <div className={styles.followUs}>
+        <div className={styles.buttonSeparator}>
+          <div className={styles.callToSocialMedia} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
